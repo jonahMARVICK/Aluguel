c15297e260f100d509fd07f13db0c61f577cc37f
jonahMARVICK <161093215+jonahMARVICK@users.noreply.github.com>
2025-12-01 17:55:58 -0300

    Delete Aluguel directory

--- Aluguel/Tipos/IndisponivelException.java
+++
@@ -1,5 +0,0 @@
-package Tipos;
-
-public class IndisponivelException extends Exception {
-    public IndisponivelException(String msg) { super(msg); }
-}
--- Aluguel/main.java
+++
@@ -1,8 +0,0 @@
-import ui.TelaMain;
-import javax.swing.SwingUtilities;
-
-public class main {
-public static void main(String[] args) {
-SwingUtilities.invokeLater(() -> new TelaMain().setVisible(true));
-}
-}
